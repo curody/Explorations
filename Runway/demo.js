433cf2db05d37fccaf6665b1cc0d3800bea997bb
curody <2687284+curody@users.noreply.github.com>
2021-12-30 18:36:22 -0500

    Remove blank line

--- Runway/demo.js
+++ Runway/demo.js
@@ -6,6 +6,5 @@ import { Runway } from './runway.mjs';
  * @example <caption>Calculate time elapsed with yearly inflation at 2%, yearly withdrawal rate of 25k, initial balance of 100k, and ending balance of 10k</caption>
  * node .\demo.js -0.02 25000 100000 10000
  */
-
 const args = process.argv.slice(2);
 Runway.printCalcs(args[0], args[1], args[2], args[3]);
